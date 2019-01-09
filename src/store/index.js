38832e4879c6ab1f69fb6e9613a189b5b88911fc
Wudilalala <569697741@qq.com>
2019-01-09 09:49:36 +0800

    wudi01

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+// import {createStore,combineReducers,compose} from "redux"
+// import listReducer from "./Reducers/listReducer"
+// import tabbarReducer from "./Reducers/tabbarReducer"
+
+// import thunk from "redux-thunk"
+// import promiseThunk from "redux-promise"
+// import {applyMiddleware} from "redux"
+
+// const reducer = combineReducers({
+// 	tabbarReducer,
+// 	listReducer
+// })
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+//     applyMiddleware(thunk,promiseThunk)
+//   ));
+
+// export default store;
